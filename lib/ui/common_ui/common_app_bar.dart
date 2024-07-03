a877a14697e66d2a6d23c78f0715809054e40025
udit-nair <uditnair90@gmail.com>
2024-07-03 21:12:12 +0530

    _appbar is removed from App Bar Builder

--- lib/ui/common_ui/common_app_bar.dart
+++ lib/ui/common_ui/common_app_bar.dart
@@ -4,7 +4,6 @@ import 'package:ezone/ui/common_ui/common_colors.dart';
 import 'package:flutter/material.dart';
 
 class CustomAppBar {
-  AppBar? _appBar = null;
   Color? _backGroundColor;
   Color? _foreGroundColor;
   String? _title;
@@ -25,7 +24,7 @@ class CustomAppBar {
   }
 
   AppBar create() {
-    return _appBar = AppBar(
+    return AppBar(
       backgroundColor: _backGroundColor,
       foregroundColor: _foreGroundColor,
       title: Text(_title ?? "DEFAULT TITLE"),
